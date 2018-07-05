837943f9a8c9837112c258a7a51cc3f5e5d6d998
chejianchao <820992376@qq.com>
2018-07-05 05:27:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,19 @@
 # Add  code here!
 
 def prime?(num)
-  for i in 2..Math.sqrt(num) 
-    
+  if num == 2
+    return true
   end
+  if num %2 ==0
+    return true
+  end
+  i = 3
+  n = Math.sqrt(num)
+  
+  while i < n
+    if( num % n == 0 )
+      return false;
+    i+=2
+  end
+  return true
 end
